bf749cf29c9bb9d395c391e7490425295b8a3c06
Vaibhav MS <vaibhavsingh4141@gmail.com>
2025-04-15 23:56:45 +0530

    Updated week 15

--- week9/React 9.2/src/App.jsx
+++ week9/React 9.2/src/App.jsx
@@ -30,24 +30,86 @@ import './App.css'
 // week 9.3
 function App(){
   return <>
-  <Parent>
+  {/* <Parent>
     <div>
       Hi I am a children of Parent Component
       <div>
         <input placeholder='Write whatever you want'></input>
       </div>
     </div>
-  </Parent>
+  </Parent> */}
+  <div onClick={() => console.log('ii')} style={{position:'fixed', left:'46px', top:'10px', width:'100px', height:'100px', backgroundColor:'red', opacity:50,zIndex:0}}></div>
+{/* <iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+<iframe
+            className="w-full h-full rounded-md"
+            src="https://www.youtube.com/embed/T-2jliitju4"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe> */}
+
   </>
 }
 
 // As we can see wrapped content of custom component can be accessed as child in parents wrapped component
-function Parent({children}){
-  return <>
-  <div style={{padding:'10px', background:'crimson', border:'0.5px solid'}}>
-  {children}
-  </div>
-  </>
-}
+// function Parent({children}){
+//   return <>
+//   <div style={{padding:'10px', background:'crimson', border:'0.5px solid'}}>
+//   {children}
+//   </div>
+//   </>
+// }
 
 export default App
