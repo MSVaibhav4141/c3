cb2113aa13cf93106c367178e59eafe492b83d10
Vaibhav MS <vaibhavsingh4141@gmail.com>
2025-06-09 23:36:56 +0530

    Debugging

--- week15/second-brain-app/src/llms/embedding.ts
+++ week15/second-brain-app/src/llms/embedding.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getEmbedding =async(text:string) =>{
   try {
     const response = await axios.post(
-      "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2/pipeline/feature-extraction",
+      "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2",
       {
         inputs: text,
       },
@@ -14,8 +14,10 @@ export const getEmbedding =async(text:string) =>{
         }
       }
     );
-   return response.data; // Embedding array
+    console.log('OK')
+    return response.data; 
   } catch (error:any) {
+    console.log('OK')
     console.error("Error fetching embedding:", error.response?.data || error.message);
   }
 }
