57a09e42f67c4b515f69148c25c92b16b82d1f70
Vaibhav MS <vaibhavsingh4141@gmail.com>
2025-06-10 11:02:00 +0530

    Checking something

--- week27/week27.2/apps/ws/index.ts
+++ week27/week27.2/apps/ws/index.ts
@@ -4,7 +4,7 @@ Bun.serve({
   port:8081,
   fetch(req, server) {
     // upgrade the request to a WebSocket
-    if (server.upgrade(req)) {
+    if (server.upgrade(req)) { //commit 
       return; // do not return a Response
     }
     return new Response("Upgrade failed", { status: 500 });
